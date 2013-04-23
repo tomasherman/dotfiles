fad2bdbd688d6d052d8847aca12ac9f7f3e88bec
Tomáš Heřman <tomas.herman@wikidi.com>
2013-04-23 12:15:13 +0200

    Refactoring in zshrc and added caps/ctrl swap

---
+++ zsh/include/basic.zsh
@@ -0,0 +1,11 @@
+unsetopt correct_all
+
+export TERM="xterm-256color"
+export EDITOR="emacsclient -c"
+alias ec="emacsclient -c"
+alias et="emacsclient -t"
+alias es="killall emacs; emacs --daemon"
+
+alias magic-devel="ssh-add; ssh tomas@magic-devel.int.wikidi.net"
+
+setxkbmap -option ctrl:swapcaps -layout us
