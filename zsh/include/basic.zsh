e3f416a0d165191f02ef6cca65594364c1141a7d
Tomáš Heřman <tomas.herman@wikidi.com>
2013-04-29 16:01:51 +0200

    no x forwarding by default

--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -4,6 +4,6 @@ alias ec="emacsclient -c"
 alias et="emacsclient -t"
 alias es="emacsclient -c '(kill emacs)'; emacs --daemon"
 
-alias magic-devel="ssh-add; ssh -AX tomas@magic-devel.int.wikidi.net"
+alias magic-devel="ssh-add; ssh -A tomas@magic-devel.int.wikidi.net"
 
 setxkbmap -option ctrl:swapcaps -layout us 2>/dev/null #setxkbmap won't be on servers
