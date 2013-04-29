d4b391903617fd5b8a0afae99f704537806aa27b
Tomas Herman <tomas.herman@wikidi.com>
2013-04-29 09:19:36 +0200

    hygienically kill emacs

--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -2,7 +2,7 @@ export TERM="xterm-256color"
 export EDITOR="emacsclient -c"
 alias ec="emacsclient -c"
 alias et="emacsclient -t"
-alias es="killall emacs; emacs --daemon"
+alias es="emacsclient -c '(kill emacs)'; emacs --daemon"
 
 alias magic-devel="ssh-add; ssh -AX tomas@magic-devel.int.wikidi.net"
 
