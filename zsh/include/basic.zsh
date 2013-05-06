960326a3d730ec7affc224b96f0a9bee2eaa7fd4
Tomas Herman <tomas.herman@gmail.com>
2013-05-06 14:09:22 +0200

    csv uses emacs now and added emacspipe

--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -1,7 +1,7 @@
 export TERM="xterm-256color"
 export EDITOR="emacsclient -c"
-alias ec="emacsclient -c"
-alias et="emacsclient -t"
+alias ec="emacsclient -c -a"
+alias et="emacsclient -t -a"
 alias es="emacsclient -c '(kill emacs)'; emacs --daemon"
 
 alias magic-devel="ssh-add; ssh -A tomas@magic-devel.int.wikidi.net"
