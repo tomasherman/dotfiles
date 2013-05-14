5e118520783647fbd86410252437d6f29cbe3184
Tomas Herman <tomas.herman@gmail.com>
2013-05-14 11:23:51 +0200

    fixed some emacs bollocks

--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -1,8 +1,8 @@
 export TERM="xterm-256color"
 export EDITOR="emacsclient -c"
-alias ec="emacsclient -c -a"
-alias et="emacsclient -t -a"
-alias es="emacsclient -c '(kill emacs)'; emacs --daemon"
+alias ec="emacsclient -a -c"
+alias et="emacsclient -a -t"
+alias es="emacsclient -e '(kill emacs)'; emacs --daemon"
 
 alias magic-devel="ssh-add; ssh -A tomas@magic-devel.int.wikidi.net"
 
