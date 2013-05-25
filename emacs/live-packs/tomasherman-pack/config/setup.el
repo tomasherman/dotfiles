958e048f38a5067a9525d37c549f46099925aa4a
Tomas Herman <tomas.herman@gmail.com>
2013-05-25 22:31:18 +0200

    projectile and some refactoring

---
+++ emacs/live-packs/tomasherman-pack/config/setup.el
@@ -0,0 +1,7 @@
+(add-hook 'prog-mode-hook 'projectile-mode)
+(add-hook 'prog-mode-hook 'electric-pair-mode)
+
+
+; setup projectile variables
+(setq projectile-cache-file "~/dotfiles/emacs/.projectile.cache")
+(setq projectile-known-projects-file "~/dotfiles/emacs/.projectile.bookmarks")
