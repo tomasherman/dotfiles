50e3846a2c5fe1ef5affcbc8ac8eeffcb475d40f
Tomas Herman <tomas.herman@gmail.com>
2013-05-26 09:49:04 +0200

    added ~/bin to pash

--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -7,3 +7,5 @@ alias es="emacsclient -e '(kill-emacs)'; emacs --daemon"
 alias magic-devel="ssh-add; ssh -A tomas@magic-devel.int.wikidi.net"
 
 setxkbmap -option ctrl:swapcaps -layout us 2>/dev/null #setxkbmap won't be on servers
+
+export PATH=$PATH:$HOME/bin
