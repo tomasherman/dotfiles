99619b86b12592ffe6aae3e6bdb7d9585b923bf6
Tomas <tomas.herman@gmail.com>
2014-09-30 11:01:02 +0200

    Added zima machine

---
+++ zsh/host-specific/tomas@Tomass-MacBook-Air.local.zsh
@@ -0,0 +1,4 @@
+alias mnt-zima="sshfs -p 22 -o volname=zima tomash@zima.int.wikidi.net:/home/tomash ~/mnt/zima"
+alias mnt-zima-root="sshfs -p 22 -o volname=zima-root tomash@zima.int.wikidi.net:/ ~/mnt/zima-root"
+
+alias umount-all="umount -f ~/mnt/*"
--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -5,6 +5,7 @@ et () { emacsclient -t $1 & }
 alias es="emacsclient -e '(kill-emacs)'; emacs --daemon"
 
 alias magic-devel="ssh-add; ssh -A tomas@magic-devel.int.wikidi.net"
+alias zima="ssh-add;ssh -A tomash@zima.int.wikidi.net"
 
 setxkbmap -option ctrl:swapcaps -layout us 2>/dev/null #setxkbmap won't be on servers
 
