52fb126b36861e15aa47b7a67b114c5758ecfd93
Tomas Herman <tomas.herman@gmail.com>
2014-11-28 12:44:44 +0100

    stuff

--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -4,9 +4,20 @@ ec () { emacsclient -c $1 & }
 et () { emacsclient -t $1 & }
 alias es="emacsclient -e '(kill-emacs)'; emacs --daemon"
 
-alias magic-devel="ssh-add; ssh -A tomas@magic-devel.int.wikidi.net"
 alias zima="ssh-add;ssh -A tomash@zima.int.wikidi.net"
+alias chicago="ssh golem@chicago.wikidi.net"
+alias cisar="ssh -A golem@cisar.int.wikidi.net"
+alias wikidi="cd ~/workspace/wikidi"
+
+PROJECT_PATHS=(~/workspace/wikidi ~/workspace)
 
 setxkbmap -option ctrl:swapcaps -layout us 2>/dev/null #setxkbmap won't be on servers
 
 export PATH=$PATH:$HOME/bin
+export GRADLE_OPTS='-Dorg.gradle.daemon=true'
+
+upload() {
+    name=$1
+    shift
+    scp $@ golem@$name.wikidi.net:'~';
+}
