fc247cf5f34d85e24535e7f7c0c1433259648d16
Tomas Herman <tomas.herman@gmail.com>
2015-07-08 09:54:34 +0200

    added new remote server

--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -9,6 +9,7 @@ alias es="emacsclient -e '(kill-emacs)'; emacs --daemon"
 alias zima="ssh-add;ssh -A tomash@zima.int.wikidi.net"
 alias chicago="ssh -A golem@chicago.wikidi.net"
 alias cisar="ssh -A golem@cisar.int.wikidi.net"
+alias sosac="ssh -A golem@sosac.int.wikidi.net"
 alias michigan="ssh -A golem@michigan.wikidi.net"
 alias golem="ssh -A golem@golem.int.wikidi.net"
 
