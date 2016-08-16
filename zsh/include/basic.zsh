b60949b386e190dd80bb90d71f7ffb3e1058773d
Tomas Herman <hermant@avast.com>
2016-08-16 11:36:43 +0200

    refinements and updates

--- zsh/include/basic.zsh
+++ zsh/include/basic.zsh
@@ -1,10 +1,12 @@
 export TERM="xterm-256color"
 export EDITOR="emacsclient -c"
-ec () { emacsclient -c $@ & }
-et () { emacsclient -t $@ }
+ec () { emacsclient -a '' -c $@ & }
+et () { emacsclient -a '' -t $@ }
 
 alias deft="ec --eval '(deft)'"
 alias es="emacsclient -e '(kill-emacs)'; emacs --daemon"
+alias es-nore="emacs --daemon"
+
 
 alias zima="ssh-add;ssh -A tomash@zima.int.wikidi.net"
 alias chicago="ssh -A golem@chicago.wikidi.net"
